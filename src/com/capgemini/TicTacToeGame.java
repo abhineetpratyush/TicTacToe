4973c369bff1af3a3a49220c89b2a1a313438c8b
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-08 10:08:13 +0530

    [abhineet] refactor : refactor to not use static char array for board

--- src/com/capgemini/TicTacToeGame.java
+++ src/com/capgemini/TicTacToeGame.java
@@ -2,15 +2,15 @@ package com.capgemini;
 
 public class TicTacToeGame {
 	private static final char EMPTY = ' ';
-	private static char[] ticTacToeBoard;
 
 	//uc1
-	public static void createBoard() {
-		ticTacToeBoard = new char[10];
+	private static char[] createBoard() {
+		char [] ticTacToeBoard = new char[10];
 		for(int i = 1; i < ticTacToeBoard.length; i++) 
 			ticTacToeBoard[i] = EMPTY;
+		return ticTacToeBoard;
 	}
-	
+
 	public static void main (String[] args) {
 		createBoard();
 	}	
