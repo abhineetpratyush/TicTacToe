35d2513e643a994b272877683f549e0f0028ee9a
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-08 19:35:23 +0530

    [abhineet] refactor : refactor to determine after every move the winner, tie or change the turn

--- src/com/capgemini/TicTacToeGame.java
+++ src/com/capgemini/TicTacToeGame.java
@@ -2,7 +2,7 @@ package com.capgemini;
 
 import java.util.Scanner;
 
-enum FirstTurn{
+enum PlayerNames{
 	PLAYER, COMPUTER
 }
 
@@ -58,12 +58,12 @@ public class TicTacToeGame {
 	 * @param moveIndex
 	 * @return
 	 */
-	private static int checkFree(char[] ticTacToeBoard, char chosenLetter) {
+	private static int checkFree(char[] ticTacToeBoard, char chosenLetter, String lastPlayer) {
 		Scanner takeInput = new Scanner(System.in);
 		int moveIndex;
 		boolean emptyStatus;
 		do {
-			System.out.println("Enter index to place letter " + chosenLetter);
+			System.out.println("Enter index to place letter " + chosenLetter + " for " + lastPlayer);
 			moveIndex = takeInput.nextInt();
 			if(ticTacToeBoard[moveIndex] == EMPTY) {
 				emptyStatus = true;
@@ -94,19 +94,85 @@ public class TicTacToeGame {
 	private static String getWhoPlaysFirst() {
 		int randomInt = (int)Math.floor((Math.random()*10)%2);
 		if(randomInt == HEADS)
-			return  FirstTurn.PLAYER.name();
+			return  PlayerNames.PLAYER.name();
 		else
-			return FirstTurn.COMPUTER.name();
+			return PlayerNames.COMPUTER.name();
 	}
 
+	/**
+	 * uc7
+	 * @param ticTacToeBoard
+	 * @param chosenLetter
+	 * @param lastPlayer
+	 * @return
+	 */
+	private static String gameManager(char[] ticTacToeBoard, char chosenLetter, String lastPlayer) {
+		int counter = 0;
+		for(int i = 1; i <=9; i++) 
+			if(ticTacToeBoard[i] == EMPTY)
+				counter++;
+		if(counter == 0) {
+			System.out.println("The game is a TIE!");
+			return "tie";
+		}
+		else if((ticTacToeBoard[1] == chosenLetter && ticTacToeBoard[2] == chosenLetter && ticTacToeBoard[3] == chosenLetter) || 
+				(ticTacToeBoard[4] == chosenLetter && ticTacToeBoard[5] == chosenLetter && ticTacToeBoard[6] == chosenLetter) || 
+				(ticTacToeBoard[7] == chosenLetter && ticTacToeBoard[8] == chosenLetter && ticTacToeBoard[9] == chosenLetter) || 
+				(ticTacToeBoard[1] == chosenLetter && ticTacToeBoard[4] == chosenLetter && ticTacToeBoard[7] == chosenLetter) ||
+				(ticTacToeBoard[2] == chosenLetter && ticTacToeBoard[5] == chosenLetter && ticTacToeBoard[8] == chosenLetter) ||
+				(ticTacToeBoard[3] == chosenLetter && ticTacToeBoard[6] == chosenLetter && ticTacToeBoard[9] == chosenLetter) ||
+				(ticTacToeBoard[1] == chosenLetter && ticTacToeBoard[5] == chosenLetter && ticTacToeBoard[9] == chosenLetter) ||
+				(ticTacToeBoard[3] == chosenLetter && ticTacToeBoard[5] == chosenLetter && ticTacToeBoard[7] == chosenLetter)) {
+			System.out.println(lastPlayer + " WON!");
+			return "win";
+		}
+		else 
+			return "change";
+	}
+	
+	private static char swapPlayerLetter(char chosenLetter) {
+		if(chosenLetter == CHARACTER_X)
+			chosenLetter = CHARACTER_O;
+		else
+			chosenLetter = CHARACTER_X;
+		return chosenLetter;
+	}
+	
+	private static String swapPlayerTurn(String lastPlayer) {
+		if(lastPlayer.contains(PlayerNames.PLAYER.name()))
+			lastPlayer = PlayerNames.COMPUTER.name();
+		else
+			lastPlayer = PlayerNames.PLAYER.name();
+		return lastPlayer;
+	}
+	
 	public static void main (String[] args) {
 		Scanner takeInput = new Scanner(System.in);
 		char[] ticTacToeBoard = createBoard();
-		System.out.println("Player letter (X or O): ");
+		System.out.println("Player letter: ");
 		char playerLetter = takeInput.next().charAt(0);
 		char computerLetter = selectLetter(playerLetter);
 		System.out.println("Computer Letter: " + computerLetter);
 		String firstChance = getWhoPlaysFirst();
+		char chosenLetter;
+		if(firstChance.contains("PLAYER"))
+			chosenLetter = playerLetter;
+		else
+			chosenLetter = computerLetter;
 		System.out.println("First Chance for " + firstChance);
+		System.out.println("\n--Initial status of board--");
+		showBoard(ticTacToeBoard);
+		String gameStatus;
+		String lastPlayer = firstChance;
+		do {
+			int moveIndex = checkFree(ticTacToeBoard, chosenLetter, lastPlayer);
+			makeMove(ticTacToeBoard, chosenLetter, moveIndex);
+			System.out.println("\n--Updated board after the move--");
+			showBoard(ticTacToeBoard);
+			gameStatus = gameManager(ticTacToeBoard, chosenLetter, lastPlayer);
+			chosenLetter = swapPlayerLetter(chosenLetter);
+			lastPlayer = swapPlayerTurn(lastPlayer);
+		}while(gameStatus.contains("change"));
+
 	}	
 }
